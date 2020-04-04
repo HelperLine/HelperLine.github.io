f82c4f769aab505c9715fb6cf04e1df769d331e2
Moritz Makowski <makowskimoritz@gmail.com>
2020-04-04 14:17:32 +0200

    Adde website frame

---
+++ src/App/Page.jsx
@@ -0,0 +1,81 @@
+import React, {useState} from 'react';
+import ProjectImage from '../images/Drawing_Logo_Project.svg';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+import clsx from 'clsx';
+
+import './Page.scss';
+import Container from "@material-ui/core/Container";
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    },
+    tab: {
+        minWidth: 125,
+    },
+    contentContainer: {
+        marginTop: 25,
+    }
+});
+
+
+export const Page = (props) => {
+
+    const classes = useStyles();
+
+    let [tab, setTab] = useState({tab: "vision"});
+
+    let tabContent;
+
+    switch (tab.tab) {
+        case "vision":
+            tabContent = "Vision";
+            break;
+        case "details":
+            tabContent = "Details";
+            break;
+        case "progress":
+            tabContent = "Progress";
+            break;
+        case "jobs":
+            tabContent = "Jobs";
+            break;
+        default:
+            tabContent = "";
+            break;
+    }
+
+    return (
+        <div>
+            <div className="BannerWindow">
+                <img src={ProjectImage} alt="Project Logo" className="Image"/>
+                <Paper className={clsx(classes.root, "TabPaper")}>
+                    <Tabs
+                        value={tab.tab}
+                        onChange={(event, value) => setTab({tab: value})}
+                        indicatorColor="secondary"
+                        textColor="secondary"
+                        centered
+                        variant="scrollable"
+                    >
+                        <Tab value="vision" label="Vision" className={classes.tab}/>
+                        <Tab value="details" label="Details" className={classes.tab}/>
+                        <Tab value="progress" label="Progress" className={classes.tab}/>
+                        <Tab value="jobs" label="Jobs" className={classes.tab}/>
+                    </Tabs>
+                </Paper>
+            </div>
+            <Container maxWidth="lg" className="ContentContainer">
+                <div className="ContentFlexBox">
+                    {tabContent}
+                </div>
+            </Container>
+        </div>
+    );
+};
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {Themer} from './Wrapper/Themer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Themer/>,
   document.getElementById('root')
 );
 
